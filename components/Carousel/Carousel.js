9634ed59c39adbc8bc7a3b3290375e505dd3ea98
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-09-13 23:06:35 -0400

    Started working on Carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,62 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselCreator(image) {
+  // Create all elements
+  const carousel = document.createElement("div");
+  const leftButton = document.createElement("div");
+  const mountainsImg = document.createElement("img");
+  const computerImg = document.createElement("img");
+  const treesImg = document.createElement("img");
+  const turntableImg = document.createElement("img");
+  const rightButton = document.createElement("div");
+  const images = [mountainsImg, computerImg, treesImg, turntableImg];
+
+  // Append Children to Parents
+  carousel.appendChild(leftButton);
+  carousel.appendChild(mountainsImg);
+  carousel.appendChild(computerImg);
+  carousel.appendChild(treesImg);
+  carousel.appendChild(turntableImg);
+  carousel.appendChild(rightButton);
+
+  // Apply Styling
+  carousel.classList.add("carousel");
+  leftButton.classList.add("left-button");
+  rightButton.classList.add("right-button");
+
+  // Add Content
+  leftButton.textContent = "<";
+  rightButton.textContent = ">";
+  mountainsImg.setAttribute("src", "./assets/carousel/mountains.jpeg");
+  computerImg.setAttribute("src", "./assets/carousel/computer.jpeg");
+  treesImg.setAttribute("src", "./assets/carousel/trees.jpeg");
+  turntableImg.setAttribute("src", "./assets/carousel/turntable.jpeg");
+
+  // Add Event Listeners
+
+  rightButton.addEventListener("click", e => {
+    let slideIndex = 1;
+    // if (e > images.length) {
+    //   slideIndex = 1;
+    // }
+    // if (e < 1) {
+    //   slideIndex = images.length;
+    // }
+    // for (i = 0; i < images.length; i++) {
+    //   images[i].style.display = "none";
+    // }
+    images[slideIndex - 1].style.display = "block";
+  });
+
+  // leftButton.addEventListener("click", e => {
+  //   let slideIndex = 1;
+  // });
+
+  return carousel;
+}
+const mountainsImg = document.createElement("img");
+const carouselContainer = document.querySelector(".carousel-container");
+
+carouselContainer.appendChild(carouselCreator(mountainsImg));
