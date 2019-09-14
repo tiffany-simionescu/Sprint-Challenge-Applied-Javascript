3c079b55f1116536c0026db50cb44c4574c06435
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-09-13 21:40:49 -0400

    Finished Tabs section

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,48 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsDiv = document.querySelector(".topics");
+
+// Asynchronous Function
+myTopics = async url => {
+  try {
+    let response = await axios.get(url);
+    response.data.topics.forEach(item => {
+      topicsDiv.appendChild(topicTabs(item));
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+myTopics("https://lambda-times-backend.herokuapp.com/topics");
+
+// HTTP GET request
+//
+// axios
+//   .get("https://lambda-times-backend.herokuapp.com/topics")
+
+//   .then(response => {
+//     console.log(response);
+
+//     response.data.topics.forEach(item => {
+//       topicsDiv.appendChild(topicTabs(item));
+//     });
+//   })
+//   .catch(function(err) {
+//     console.log(err, "There Is An Error");
+//   });
+
+function topicTabs(obj) {
+  // Create all elements
+  const tab = document.createElement("div");
+
+  // Apply Style
+  tab.classList.add("tab");
+
+  // Add Content
+  tab.textContent = obj;
+
+  return tab;
+}
